bcacef986af99466e4e89aad8bdc1314946b4f0b
hangttp25082001 <hanghien25.8@gmail.com>
2024-01-02 17:44:23 +0700

    Full flow payment - Moon2508

---
+++ cypress/e2e/payment.spec.cy.js
@@ -0,0 +1,229 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const randomNum = getRandomInt(1, 100);
+// get date
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1; // Month is zero-based, so add 1
+const day = currentDate.getDate();
+
+// Format the date as per your requirements
+const formattedDate = `${day}${month}${year}`;
+
+function xmlProperty(xml, property) {
+  return Cypress.$(Cypress.$.parseXML(xml)).find(property).text();
+}
+const url_base = "https://haloship.imediatech.com.vn:8087/ItopupService1.4/services/TopupInterface?wsdl";
+const username ="IMEDIA_TEST";
+const password ="24112536637251";
+const keyBirthdayTime ='2022/11/29 09:26:01.690';
+const requestID = 'HangPTDV_'+ randomNum + randomNum +'_'+ formattedDate ;
+
+
+describe('SOAP API Testing - FULL Flow Transaction', () => {
+  beforeEach(()=>
+  {
+    cy.request({
+      method: 'POST',
+      url: url_base,
+      headers: {
+        'Content-Type': 'text/xml',
+        
+        SOAPAction: '',
+      },
+      body: `
+      <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+      <Body>
+          <signInAsPartner xmlns="http://interfaces.itopup.vnptepay.vn">
+              <username>${username}</username>
+              <password>${password}</password>
+          </signInAsPartner>
+      </Body>
+  </Envelope>
+      `,
+    }).then((response) => {
+      // Kiểm tra phản hồi
+      expect(response.status).to.eq(200);
+      cy.log(response.body);
+     
+
+      
+    
+      const errorCode = xmlProperty(response.body, 'errorCode');
+      const errorMessage = xmlProperty(response.body, 'errorMessage');
+      const token = xmlProperty(response.body, 'token') ;
+      // cy.wrap('token').as('token');
+      // cy.setToken(token);
+      Cypress.env('token', token);
+      cy.log(errorCode);
+      expect(errorCode).to.eq('0');
+      // cy.log(errorMessage);
+      cy.log(token);
+  });
+});
+  it.skip('execute download softpin - 1000 ', () => {
+    const token = Cypress.env('token');
+    cy.log(token)
+      cy.request({
+      method: 'POST',
+      url: url_base,
+      headers: {
+        'Content-Type': 'text/xml',
+        
+        SOAPAction: '',
+      },
+      body: `
+      <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+    <Body>
+        <partnerDownloadSoftpinV10 xmlns="http://interfaces.itopup.vnptepay.vn">
+            <username>${username}</username>
+            <buyItems>
+            <buyItem>
+            <quantity>1</quantity>
+            <productId>1
+            </productId>
+            </buyItem>
+</buyItems>
+            <requestID>${requestID}</requestID>
+            <keyBirthdayTime>${keyBirthdayTime}</keyBirthdayTime>
+            <token>${token}</token>
+        </partnerDownloadSoftpinV10>
+    </Body>
+</Envelope>
+      `,
+    }).then((response) => {
+      // Kiểm tra phản hồi
+      expect(response.status).to.eq(200);
+      cy.log(response.body);
+      const errorMessage = xmlProperty(response.body, 'errorMessage');
+      const errorCode = xmlProperty(response.body, 'errorCode');
+      cy.log(errorMessage);
+      cy.log('Giao dịch '+requestID + ' Thành công ');
+      expect(errorCode).to.eq('0');
+
+    });
+    
+  });
+  it.skip('execute topup transaction - 1200', () => {
+    const token = Cypress.env('token');
+    cy.log(token)
+      cy.request({
+      method: 'POST',
+      url: url_base,
+      headers: {
+        'Content-Type': 'text/xml',
+        
+        SOAPAction: '',
+      },
+      body: `
+      <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+    <Body>
+        <partnerDirectTopupGame xmlns="http://interfaces.itopup.vnptepay.vn">
+            <username>${username}</username>
+            <providerCode>Viettel</providerCode>
+            <targetAccount>0982345678 </targetAccount>
+            <amount>20000</amount>
+            <requestID>${requestID}</requestID>
+            <token>${token}</token>
+        </partnerDirectTopupGame>
+    </Body>
+</Envelope>
+      
+      `,
+    }).then((response) => {
+      // Kiểm tra phản hồi
+      expect(response.status).to.eq(200);
+      cy.log(response.body);
+      const errorMessage = xmlProperty(response.body, 'errorMessage');
+      const errorCode = xmlProperty(response.body, 'errorCode');
+      cy.log(errorMessage);
+      cy.log('Giao dịch '+requestID + ' Thành công ');
+      expect(errorCode).to.eq('0');
+
+    });
+    
+  });
+
+  it('Check topup transaction - 1300', () => {
+    const token = Cypress.env('token');
+    cy.log(token)
+      cy.request({
+      method: 'POST',
+      url: url_base,
+      headers: {
+        'Content-Type': 'text/xml',
+        
+        SOAPAction: '',
+      },
+      body: `
+      <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+    <Body>
+        <getDirectTransDetail xmlns="http://interfaces.itopup.vnptepay.vn">
+            <username>${username}</username>
+            <requestID>HangPTDV_9696_212024</requestID>
+            <token>${token}</token>
+        </getDirectTransDetail>
+    </Body>
+</Envelope>
+      
+      `,
+    }).then((response) => {
+      // Kiểm tra phản hồi
+      expect(response.status).to.eq(200);
+      cy.log(response.body);
+      const errorMessage = xmlProperty(response.body, 'errorMessage');
+      const errorCode = xmlProperty(response.body, 'errorCode');
+      cy.log(errorMessage);
+      cy.log('Giao dịch Thành công ');
+      expect(errorCode).to.eq('0');
+
+    });
+    
+  });
+
+  it('Redownload transaction - 1100', () => {
+    const token = Cypress.env('token');
+    cy.log(token)
+      cy.request({
+      method: 'POST',
+      url: url_base,
+      headers: {
+        'Content-Type': 'text/xml',
+        
+        SOAPAction: '',
+      },
+      body: `
+      <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+    <Body>
+        <partnerRedownloadSoftpin xmlns="http://interfaces.itopup.vnptepay.vn">
+            <username>${username}</username>
+            <requestId>HangPTDV_6767_212024</requestId>
+            <keyBirthdayTime>${keyBirthdayTime}</keyBirthdayTime>
+            <token>${token}</token>
+        </partnerRedownloadSoftpin>
+    </Body>
+</Envelope>
+      
+      `,
+    }).then((response) => {
+      // Kiểm tra phản hồi
+      expect(response.status).to.eq(200);
+      cy.log(response.body);
+      const errorMessage = xmlProperty(response.body, 'errorMessage');
+      const errorCode = xmlProperty(response.body, 'errorCode');
+      cy.log(errorMessage);
+      cy.log('Giao dịch Thành công ');
+      expect(errorCode).to.eq('0');
+
+    });
+    
+  });
+
+
+
+});
+
